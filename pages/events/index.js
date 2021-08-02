2f5e36dfa7d4dcb0fc3e4fca6d8143cfabe51884
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-08-02 23:34:27 +0600

    pagination added

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,10 @@
 import EventItem from "@/components/EventItem";
 import Layout from "@/components/Layout";
+import Pagination from "@/components/Pagination";
 import { API_URL } from "@/config/index";
+const PER_PAGE = 1;
 
-export default function EventsPage({ events }) {
-  console.log(events);
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout>
       <h1>All Events</h1>
@@ -13,18 +14,28 @@ export default function EventsPage({ events }) {
       {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} PER_PAGE={PER_PAGE} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`);
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE;
+  const res = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
+  );
   const events = await res.json();
+
+  // Find total
+  const totalRes = await fetch(`${API_URL}/events/count`);
+  const total = await totalRes.json();
 
   return {
     props: {
       events,
+      page: +page,
+      total,
     },
-    revalidate: 1,
   };
 }
