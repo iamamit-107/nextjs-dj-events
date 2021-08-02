ace03764f1750547945b63d5f934abb70dfabab9
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-08-02 15:51:37 +0600

    edit event done

---
+++ pages/events/edit/[id].js
@@ -0,0 +1,172 @@
+import moment from "moment";
+import Layout from "@/components/Layout";
+import styles from "@/styles/Form.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { API_URL } from "@/config/index";
+import Image from "next/image";
+import { FaImage } from "react-icons/fa";
+
+export default function AddEvent({ evt }) {
+  const [values, setValues] = useState({
+    name: evt.name,
+    performers: evt.performers,
+    venue: evt.venue,
+    address: evt.address,
+    date: evt.date,
+    time: evt.time,
+    description: evt.description,
+  });
+
+  const [imagePreview, setImagePreview] = useState(
+    evt ? evt.image.formats.thumbnail.url : null
+  );
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const hasEmptyValue = Object.values(values).some(
+      (element) => element === ""
+    );
+
+    if (hasEmptyValue) {
+      toast.error("Please Fill all the fields");
+    }
+
+    const res = await fetch(`${API_URL}/events/${evt.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    if (!res.ok) {
+      toast.error("Something went wrong");
+    } else {
+      const evt = await res.json();
+      router.push(`/events/${evt.slug}`);
+    }
+  };
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  return (
+    <Layout title="Add new events">
+      <ToastContainer />
+
+      <Link href="/events">
+        <a>Go Back</a>
+      </Link>
+      <h1>Edit Events</h1>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor="name">Event Name</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              value={values.name}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="performers">Performers</label>
+            <input
+              type="text"
+              name="performers"
+              id="performers"
+              value={values.performers}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="venue">Venue</label>
+            <input
+              type="text"
+              name="venue"
+              id="venue"
+              value={values.venue}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              name="address"
+              id="address"
+              value={values.address}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="date">Date</label>
+            <input
+              type="date"
+              name="date"
+              id="date"
+              value={moment(values.date).format("yyyy-MM-DD")}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="time">Time</label>
+            <input
+              type="text"
+              name="time"
+              id="time"
+              value={values.time}
+              onChange={handleChangeInput}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="description">Event Description</label>
+          <textarea
+            type="text"
+            name="description"
+            id="description"
+            value={values.description}
+            onChange={handleChangeInput}
+          ></textarea>
+        </div>
+
+        <input type="submit" value="Update Event" className="btn" />
+      </form>
+
+      <h2>Image Preview</h2>
+      {imagePreview ? (
+        <Image src={imagePreview} width={250} height={170} />
+      ) : (
+        <h3>No image to preview</h3>
+      )}
+      <div>
+        <button className="btn-secondary">
+          <FaImage /> Set Image
+        </button>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ params: { id } }) {
+  const res = await fetch(`${API_URL}/events/${id}`);
+  const evt = await res.json();
+
+  return {
+    props: {
+      evt: evt,
+    },
+  };
+}
