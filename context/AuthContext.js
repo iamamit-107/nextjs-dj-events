91ba8636fcd5750c6edf5401a4a6aa544bc42565
Ahmed Faisal Amit <ahmed.amit107@gmail.com>
2021-08-03 16:55:23 +0600

    logout and destroy cookie

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -40,7 +40,14 @@ export const AuthProvider = ({ children }) => {
   };
 
   const logout = async () => {
-    console.log("logout");
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: "POST",
+    });
+
+    if (res.ok) {
+      setUser(null);
+      router.push("/");
+    }
   };
 
   const loginUserCheck = async (user) => {
